e57316bf82c0bc06927c876dd419efb2924171d6
Debojeet Karmakar <debojeetkarmakar2020@gmail.com>
2024-05-23 19:15:18 +0530

    port changed

--- Server.js
+++ Server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = 8000;
-// const PORT = process.env.PORT || 5000;
+// const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors({
     origin: '*'
