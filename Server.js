01e7edc2ad7bae68043d65e4fa1e1206920541de
Debojeet Karmakar <debojeetkarmakar2020@gmail.com>
2024-05-23 18:42:54 +0530

    added the origin

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ const PORT = 8000;
 // const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://debojeetkarmakar.vercel.app/'
 }));
 
 app.use(bodyParser.json());
